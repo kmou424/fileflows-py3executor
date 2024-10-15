9a2a5b19eb645ba2591fbeaff67504ab09ab38a4
kmou424 <me@kmou424.moe>
2024-10-15 11:39:38 +0800

    feat: add support to set working file

--- py3executor.js
+++ py3executor.js
@@ -2,9 +2,10 @@
  * @description Executes any .py file using python3. Supports parameters and setting Flow variables.
  * @param {string} PyFile Path to the .py file
  * @param {string} Args Parameters in key-value pair format, formatted as [KEY=VALUE], allowing multiple settings, e.g., [WORKDIR=/mnt/workdir] [TEMP={temp}]
+ * @param {string} SetWorkingFile Path to the file to set as the working file. Supports parameters in the format of [KEY] to replace with Flow variables at end of execution.
  * @output Success
  */
-function Script(PyFile, Args)
+function Script(PyFile, Args, SetWorkingFile)
 {
     let process = Flow.Execute({
         command: "/usr/bin/python3",
@@ -31,5 +32,31 @@ function Script(PyFile, Args)
     if (process.starndardError)
         Logger.ILog("error: ", process.starndardError);
 
+    if (SetWorkingFile && SetWorkingFile != "") {
+        let workingFile = String(SetWorkingFile);
+        let args = extractArgs(workingFile);
+        Logger.ILog("Args found in SetWorkingFile: " + args);
+        if (args.length != 0) {
+            args.forEach(arg => {
+                workingFile = workingFile.replace('['+arg+']', Variables[arg]);
+            });
+        }
+        Flow.SetWorkingFile(workingFile)
+    }
+
     return 1;
+}
+
+function extractArgs(input)
+{
+    const regex = /\[(.*?)\]/g;
+
+    const results = [];
+    let match;
+
+    while ((match = regex.exec(input)) !== null) {
+        results.push(match[1]);
+    }
+
+    return results;
 }
